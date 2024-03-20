7b5dd51f287a7c2ae82ebf7abc8b6da05b0cb597
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2024-03-19 23:07:21 -0500

    agregando etiqueta repository

--- Proy_SoftTienda/src/main/java/com/soft/tienda/repositories/UserRespository.java
+++ Proy_SoftTienda/src/main/java/com/soft/tienda/repositories/UserRespository.java
@@ -3,7 +3,9 @@ package com.soft.tienda.repositories;
 import com.soft.tienda.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRespository extends JpaRepository<Usuario, Long> {
     @Query("SELECT U FROM Usuario U WHERE U.correo=?1")
     Usuario obtenerUsuarioPorCorreo(String correo);
